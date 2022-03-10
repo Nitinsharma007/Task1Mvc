21dd78f6ec01138c434b884a5313b105a5866293
Nitinsharma007 <sharmanitin2246@gmail.com>
2022-03-11 02:58:28 +0530

    initial commit

---
+++ Task1MVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Task1MVC.Models;
+
+namespace Task1MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Employee emp = new Employee();
+            emp.empID = 454;
+            emp.empName = "Nitin";
+            emp.empEmail = "Sharmanitin2246@gmail.com";
+
+            return View(emp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Task1MVC/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1MVC.Models
+{
+    public class Employee
+    {
+        public int empID { get; set; }
+        public string empName { get; set; }
+        public string empEmail { get; set; }
+    }
+}
